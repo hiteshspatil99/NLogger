37b48501716a49d7568641f895c5b0ed3af2cac6
Hitesh <hiteshspatil99@gmail.com>
2021-12-13 22:50:03 +0530

    [Hitesh] add : NLogger Used For Taking High Quality Logs

---
+++ NLogger/NLogger/AddNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLogger
+{
+    public class AddNumbers
+    {
+        private readonly NLog nLog = new NLog();
+        public int Sum(int firstNumber, int secondNumber)
+        {
+            
+            if (firstNumber == 0 || secondNumber == 0) //Checking values equal to Zero
+            {
+                nLog.LogDebug("Debug Successfull : Sum()");
+                nLog.LogError("Expecting null values");
+                nLog.LogWarn("secondNumber or secondNumber should not equal to 0");
+            }
+            int result = secondNumber + secondNumber;
+            nLog.LogDebug("Debug Successfull : Sum()");
+            nLog.LogInfo("Sum Method passed, Result" + result);
+            return result;
+        }
+    }
+}
---
+++ NLogger/NLogger/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddNumbers result = new AddNumbers();
+            result.Sum(2, 3);
+        }
+    }
+}
